2364a7bd9382c140036e0cc2c6b3c2c1f09071c9
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2025-05-31 23:16:06 +0200

    admin page done, so as the whole backend. Only the frontend and inputchecks are left

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,21 +8,52 @@
 
     <title>Gömbi's Barbershop admin</title>
 </head>
+
 <body>
     <div class="container">
-        <a href="index.php?todo=logOut" class='btn btn-primary'>Kijelentkezés</a>    
+        <a href="index.php?todo=logOut" class='btn btn-primary'>Kijelentkezés</a>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Felhasználó email címe</th>
+                    <th>Dátum</th>
+                    <th>Idő</th>
+                    <th>Szolgáltatás</th>
+                    <th>Visszaigazolás</th>
+                    <th>Lemondás</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require_once "db.php";
+                $dbModel = new dataBase("localhost", "root", "", "idopont_php");
+                $todo = $_GET['todo'] ?? "list";
+                switch ($todo) {
+                    case "list":
+                        include_once "adminProcess.php";
+                        break;
+                    case "approve":
+                        if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+                            $id = (int)$_GET['id'];
+                            $dbModel->ApproveAppointment($id);
+                        }
+                        header("Location: admin.php");
+                        break;
+                    case "cancel":
+                        if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+                            $id = (int)$_GET['id'];
+                            $dbModel->DeleteAppointment($id);
+                        }
+                        header("Location: admin.php");
+                        break;
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
-    <?php
-        
-        $todo = $_POST['todo'] ?? "";
-        // switch($todo){
-        //     case 'logOut':
-        //         session_start();
-        //         session_unset();
-        //         session_destroy();
-        //         header("Location: index.php");
-        //         break;
-        // }
-    ?>
+
 </body>
+
 </html>
---
+++ adminProcess.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "db.php";
+$dbModel = new dataBase("localhost", "root", "", "idopont_php");
+$appointments = $dbModel->GetAppointments();
+
+foreach ($appointments as $a) {
+    echo "
+        <tr>
+            <td>$a->id</td>
+            <td>$a->email</td>
+            <td>$a->date</td>
+            <td>$a->time</td>
+            <td>$a->type</a>
+            <td>" . ($a->isApproved == 0 ? "<a href='?todo=approve&id=$a->id' class='btn btn-success'>Visszaigazolás</a>" : "Visszaigazolva") . "</td>
+            <td><a href='?todo=cancel&id=$a->id' class='btn btn-danger'>Lemondás</a></td>
+        </tr>
+        ";
+}
